e2b66c8e4cf36d4756afa10046246afa996d9511
Jake Lauer <jake.lauer27@gmail.com>
2018-10-15 11:37:26 -0600

    Attempt to connect bonus-wheel tests

--- test/Bonus-wheel-test.js
+++ test/Bonus-wheel-test.js
@@ -1,33 +1,31 @@
 const chai = require('chai');
 const expect = chai.expect;
 const BonusWheel = require('../lib/Bonus-wheel.js');
-
-
 global.Wheel = require('../lib/wheel.js');
 
-describe('Wheel', () => {
+describe('bonusWheel', () => {
 
   var bonusWheel;
 
-beforeEach(() => {
-  bonusWheel = new BonusWheel;
-});
+  beforeEach(() => {
+    bonusWheel = new BonusWheel;
+  });
 
-it('should default to having 13 elements', () => {
-  expect(bonusWheel.elements.length).to.equal(13);
-});
+  it('should default to having 13 elements', () => {
+    expect(bonusWheel.elements.length).to.equal(13);
+  });
 
-it('should keep track of the current element', () => {
-  expect(bonusWheel).to.have.property('currentElement');
-});
+  it('should keep track of the current element', () => {
+    expect(bonusWheel).to.have.property('currentElement');
+  });
 
-it('should be able to select a new current Element', () => {
-  expect(bonusWheel.elements.includes(bonusWheel.spin())).to.be.true;
-});
+  it('should be able to select a new current Element', () => {
+    expect(bonusWheel.elements.includes(bonusWheel.spin())).to.be.true;
+  });
 
-it('should be able to randomly sort its elements', () => {
-  var elements = bonusWheel.elements;
-  bonusWheel.randomSortElements()
-  expect(bonusWheel.elements).to.not.eql(elements);
-});
+  it('should be able to randomly sort its elements', () => {
+    var elements = bonusWheel.elements;
+    bonusWheel.randomSortElements()
+    expect(bonusWheel.elements).to.not.eql(elements);
+  });
 });
