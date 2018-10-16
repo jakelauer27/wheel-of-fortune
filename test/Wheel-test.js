0a05bec09eb34ab2712ef567c80815f33c64b3ff
Jake Lauer <jake.lauer27@gmail.com>
2018-10-16 14:13:18 -0600

    Finish wheel tests

--- test/Wheel-test.js
+++ test/Wheel-test.js
@@ -8,7 +8,7 @@ global.Game = require('../lib/game.js');
 global.Player = require('../lib/player.js');
 global.data = require('../lib/data.js');
 global.updateDom = require('../lib/updateDom.js');
-chai.spy.on(global.updateDom, ['resetElements', 'generateElements', 'spinWheel', 'toggleSpinButton'], () => true);
+chai.spy.on(global.updateDom, ['generateElements', 'spinWheel', 'toggleSpinButton'], () => true);
 
 describe('Wheel', () => {
 
@@ -26,18 +26,41 @@ describe('Wheel', () => {
     expect(wheel).to.have.property('currentElement');
   });
 
-  it('should be able to generate new elements on the wheel', () => {
-    wheel.generateElements(data);
-    expect(wheel.elements.length).to.equal(13);
+  describe('Wheel - generateElements()', () => {
+
+    it('should be able to randomly generate new elements on the wheel', () => {
+      wheel.generateElements(data);
+      const firstGeneration = wheel.elements;
+      expect(wheel.elements.length).to.equal(13);
+      wheel.generateElements(data);
+      expect(wheel.elements).to.not.eql(firstGeneration)
+    });
+  
+    it('should be able to append its elements to the dom wheel', () => {
+      updateDom.generateElements.__spy.calls = [];
+      wheel.generateElements(data);
+      expect(updateDom.generateElements).to.have.been.called(1)
+    });
   });
 
-  it('should be able to append its elements to the dom wheel', () => {
-    wheel.generateElements(data);
-    expect(updateDom.generateElements).to.have.been.called(2)
-  });
+  describe('Wheel - spin()', () => {
 
-  it('should be able to select a new current Element', () => {
-    wheel.generateElements(data);
-    expect(wheel.elements.includes(wheel.spin())).to.be.true;
+    it('should be able to randomly select and return a new selected Element', () => {
+      wheel.generateElements(data);
+      expect(wheel.elements.includes(wheel.spin())).to.be.true;
+      expect(wheel.spin()).to.equal(wheel.currentElement);
+    });
+
+    it('should append the new selected wheel element to the dom', () => {
+      updateDom.spinWheel.__spy.calls = [];
+      wheel.spin();
+      expect(updateDom.spinWheel).to.have.been.called(1);
+    });
+
+    it('should disable the spin wheel on the dom', () => {
+      updateDom.toggleSpinButton.__spy.calls = [];
+      wheel.spin();
+      expect(updateDom.toggleSpinButton).to.have.been.called(1);
+    });
   });
 });
